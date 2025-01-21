cbc41dd22535acbd3eeaaef5b302143675ba746e
AltafAlam Shaikh <gitforaltaf@gmail.com>
2025-01-21 22:46:10 +0530

    file explorer update

--- machine-coding-react-js/file-explorer/src/App.tsx
+++ machine-coding-react-js/file-explorer/src/App.tsx
@@ -4,7 +4,7 @@ import FileExplorer from "./FileExplorer";
 
 const App = () => {
   const [explorer, setExplorer] = useState(DATA);
-  return <FileExplorer explorer={explorer} />;
+  return <FileExplorer explorer={explorer} setExplorer={setExplorer} />;
 };
 
 export default App;
--- machine-coding-react-js/file-explorer/src/FileExplorer.tsx
+++ machine-coding-react-js/file-explorer/src/FileExplorer.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+type FileType = "file" | "folder";
 
 interface Item {
   id: string;
@@ -15,26 +17,80 @@ interface IType {
     items?: Item[];
     isEditing?: boolean;
   };
+  setExplorer: Function;
 }
 
-const FileExplorer = ({ explorer }: IType) => {
+const FileExplorer = ({ explorer, setExplorer }: IType) => {
+  const [value, setValue] = useState("");
   const [toggle, setToggle] = useState(false);
+  const [openInput, setOpenInput] = useState({
+    visible: false,
+    isFolder: false,
+  });
+
+  const handleOpenInput = (type: FileType) => {
+    setOpenInput((prev) => ({
+      ...prev,
+      visible: true,
+      isFolder: type === "folder",
+    }));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setValue(value);
+  };
+
+  const handleEnter = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    id: string,
+    type: FileType
+  ) => {
+    if (e.key !== "Enter") return;
+
+    // setExplorer(prev => (
+
+    // ))
+  };
+
   return (
     <div className="explorer">
       {explorer.isFolder ? (
         <>
-          <span
-            style={{ cursor: "pointer" }}
-            onClick={() => setToggle((prev) => !prev)}
-          >
-            📁{explorer.name}
+          <span className="folder">
+            <span
+              style={{ cursor: "pointer" }}
+              onClick={() => setToggle((prev) => !prev)}
+            >
+              📁{explorer.name}
+            </span>
+            <button onClick={() => handleOpenInput("file")}>file</button>
+            <button onClick={() => handleOpenInput("folder")}>folder</button>
           </span>
           <br />
+          {openInput.isFolder && openInput.visible && (
+            <input
+              placeholder="folder name"
+              onChange={handleChange}
+              onKeyDown={(e) => handleEnter(e, explorer.id, "folder")}
+            />
+          )}
+          {!openInput.isFolder && openInput.visible && (
+            <input
+              placeholder="file name"
+              onChange={handleChange}
+              onKeyDown={(e) => handleEnter(e, explorer.id, "file")}
+            />
+          )}
           <div
             style={{ paddingLeft: "2rem", display: toggle ? "none" : "block" }}
           >
             {explorer?.items?.map((exp) => (
-              <FileExplorer key={exp.id} explorer={exp} />
+              <FileExplorer
+                key={exp.id}
+                explorer={exp}
+                setExplorer={setExplorer}
+              />
             ))}
           </div>
         </>
