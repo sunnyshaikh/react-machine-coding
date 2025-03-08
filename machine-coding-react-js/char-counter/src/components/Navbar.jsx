e33a2120ba01d54dfcadc0fc31193f25556eb970
AltafAlam Shaikh <gitforaltaf@gmail.com>
2025-03-08 16:31:51 +0530

    navbar added

---
+++ machine-coding-react-js/char-counter/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header className="header">
+      <nav className="container">
+        <a href="#" className="text-2xl font-bold">
+          {"{ }"} Character Counter
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
